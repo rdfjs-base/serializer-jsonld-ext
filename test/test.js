16d15411e453caf12ad0d577b538c51523a1e1f8
bergi <bergi@axolotlfarm.org>
2017-08-15 22:33:24 +0200

    added skipGraphProperty option

--- test/test.js
+++ test/test.js
@@ -163,6 +163,155 @@ describe('rdf-serializer-jsonld-ext', () => {
     })
   })
 
+  it('should skip @graph property if options is true and array.length == 1', () => {
+    const root = rdf.blankNode()
+    const property = rdf.blankNode()
+
+    const quads = [
+      rdf.quad(
+        root,
+        rdf.namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
+        rdf.namedNode('http://example.org/Thing')
+      ),
+      rdf.quad(
+        root,
+        rdf.namedNode('http://example.org/property0'),
+        property
+      ),
+      rdf.quad(
+        property,
+        rdf.namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
+        rdf.namedNode('http://example.org/OtherThing')
+      ),
+      rdf.quad(
+        property,
+        rdf.namedNode('http://example.org/property1'),
+        rdf.literal('value1')
+      )
+    ]
+
+    const context = {
+      '@context': {
+        '@vocab': 'http://example.org/'
+      },
+      '@type': 'Thing'
+    }
+
+    const jsonld = {
+      '@context': {
+        '@vocab': 'http://example.org/'
+      },
+      '@id': '_:b0',
+      '@type': 'Thing',
+      property0: {
+        '@id': '_:b1',
+        '@type': 'OtherThing',
+        property1: 'value1'
+      }
+    }
+
+    const input = new Readable()
+
+    input._readableState.objectMode = true
+
+    input._read = () => {
+      quads.forEach((quad) => {
+        input.push(quad)
+      })
+
+      input.push(null)
+    }
+
+    const serializer = new JsonLdSerializerExt({frame: true, context: context, skipGraphProperty: true})
+    const stream = serializer.import(input)
+
+    let result
+
+    stream.on('data', (data) => {
+      result = data
+    })
+
+    return rdf.waitFor(stream).then(() => {
+      assert.deepEqual(result, jsonld)
+    })
+  })
+
+  it('should not skip @graph property if options is true and array.length != 1', () => {
+    const s0 = rdf.blankNode('b0')
+    const s1 = rdf.blankNode('b1')
+
+    const quads = [
+      rdf.quad(
+        s0,
+        rdf.namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
+        rdf.namedNode('http://example.org/Thing')
+      ),
+      rdf.quad(
+        s0,
+        rdf.namedNode('http://example.org/property0'),
+        rdf.literal('value0')
+      ),
+      rdf.quad(
+        s1,
+        rdf.namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
+        rdf.namedNode('http://example.org/OtherThing')
+      ),
+      rdf.quad(
+        s1,
+        rdf.namedNode('http://example.org/property1'),
+        rdf.literal('value1')
+      )
+    ]
+
+    const context = {
+      '@context': {
+        '@vocab': 'http://example.org/'
+      }
+    }
+
+    const jsonld = {
+      '@context': {
+        '@vocab': 'http://example.org/'
+      },
+      '@graph': [{
+        '@id': '@default'
+      }, {
+        '@id': '_:b0',
+        '@type': 'Thing',
+        'property0': 'value0'
+      }, {
+        '@id': '_:b1',
+        '@type': 'OtherThing',
+        'property1': 'value1'
+      }]
+    }
+
+    const input = new Readable()
+
+    input._readableState.objectMode = true
+
+    input._read = () => {
+      quads.forEach((quad) => {
+        input.push(quad)
+      })
+
+      input.push(null)
+    }
+
+    const serializer = new JsonLdSerializerExt({frame: true, context: context, skipGraphProperty: true})
+    const stream = serializer.import(input)
+
+    let result
+
+    stream.on('data', (data) => {
+      result = data
+    })
+
+    return rdf.waitFor(stream).then(() => {
+      assert.deepEqual(result, jsonld)
+    })
+  })
+
   it('should support prefixes', () => {
     const quad = rdf.quad(
       rdf.namedNode('http://example.org/subject'),
